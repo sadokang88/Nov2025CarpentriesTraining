da963d7852cec3de2434c928c50254a09269df8b
Sa Do Kang <sdk004@health.ucsd.edu>
2025-11-05 09:51:23 -0800

    committing two different files at same time

--- cod.R
+++ cod.R
@@ -1,3 +1,6 @@
 x <- 10
 y <- x + 3
 z <- y - x
+
+#idea for analysis
+z + y + x
--- code.py
+++ code.py
@@ -2,3 +2,8 @@ x = 5
 y = x + 2
 
 #note to self be sure to update code
+
+import pandas as pd
+
+#try same analysis as in R
+x + y
